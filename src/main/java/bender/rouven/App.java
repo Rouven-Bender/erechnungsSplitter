e8ee2e872663bd5628a9d358ca6e598c0409edb2
kyu <rouven-pascal.bender@gmx.de>
2025-06-20 15:28:29 +0200

    initial java code

---
+++ src/main/java/bender/rouven/App.java
@@ -0,0 +1,13 @@
+package bender.rouven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
