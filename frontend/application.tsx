e1f069f383b5ae03d2ceffa59557dd1a28c76534
kyu <rouven-pascal.bender@gmx.de>
2025-06-27 16:40:18 +0200

    got a pdf page to render in the webview

--- frontend/application.tsx
+++ frontend/application.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import {createRoot} from 'react-dom/client';
 
 import FileTree from "./components/filetree";
+import { Document, Page } from 'react-pdf';
+
+import { pdfjs } from 'react-pdf';
+
 
 function Application(){
+    pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 	return (
 		<div className="grid grid-cols-2 min-h-screen">
-			<div className="h-full bg-blue-500 p-4">Left Div</div>
+			<div className="h-full bg-blue-500 p-4">
+                <Document file="/pdf">
+                    <Page pageNumber={1}/>
+                </Document>
+            </div>
 			<div className="h-full bg-green-500 p-4">Right Div</div>
 		</div>
 	)
