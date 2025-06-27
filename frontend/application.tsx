87fb45dd8582b53c2e88df38ef30aecfac92223e
kyu <rouven-pascal.bender@gmx.de>
2025-06-27 16:29:37 +0200

    state before adding pdf viewer

--- frontend/application.tsx
+++ frontend/application.tsx
@@ -5,8 +5,9 @@ import FileTree from "./components/filetree";
 
 function Application(){
 	return (
-		<div>
-		<FileTree />
+		<div className="grid grid-cols-2 min-h-screen">
+			<div className="h-full bg-blue-500 p-4">Left Div</div>
+			<div className="h-full bg-green-500 p-4">Right Div</div>
 		</div>
 	)
 }
--- src/main/java/rouven/bender/erechnungssplitter/api.java
+++ src/main/java/rouven/bender/erechnungssplitter/api.java
@@ -1,9 +1,12 @@
 package rouven.bender.erechnungssplitter;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,26 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class RestAPI {
-    private File f;
+    private File path;
     private String basepath;
     RestAPI() {
-        basepath = System.getProperty("user.home");
-        f = new File(basepath);
+        //TODO: Make this configurable
+        basepath = Paths.get(System.getProperty("user.home"), "src", "erechnungsSplitter").toString();
+        path = new File(basepath);
     }
 
     @GetMapping("/reset")
     void reset(){
-        f = new File(basepath);
+        path = new File(basepath);
     }
     
     @GetMapping("/pwd")
     String pwd(){
-        return f.toString();
+        return path.toString();
     }
 
     @GetMapping("/ls")
     String[] ls(){
-        File[] fs = f.listFiles(File::isDirectory);
+        File[] fs = path.listFiles(File::isDirectory);
         if (fs == null) {
             return null;
         }
@@ -43,7 +47,19 @@ class RestAPI {
     }
     @PostMapping("/open")
     void open(@RequestBody String directoryname){
-        Path p = Paths.get(f.toString(), directoryname).normalize();
-        f = new File(p.toUri());
+        Path p = Paths.get(path.toString(), directoryname).normalize();
+        path = new File(p.toUri());
+    }
+
+    @GetMapping(path = "/pdf", produces = "application/pdf")
+    ResponseEntity<byte[]> pdf() {
+        File filename = new File(Paths.get(path.toString(), "mustang.pdf").toString());
+        try (FileInputStream fis = new FileInputStream(filename)){ 
+            byte[] out = fis.readAllBytes();
+            return ResponseEntity.ok().cacheControl(CacheControl.noCache()).body(out);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(new byte[]{});
+        }
     }
 }
