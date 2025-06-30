36355782d9c4e473b124817d17a7d060237d6a05
kyu <rouven-pascal.bender@gmx.de>
2025-06-30 15:02:28 +0200

    moved: pdf display component

--- frontend/application.tsx
+++ frontend/application.tsx
@@ -2,15 +2,9 @@ import React, {useState, useEffect} from "react";
 import {createRoot} from 'react-dom/client';
 
 //import FileTree from "./components/filetree";
-import { Document, Page } from 'react-pdf';
+import { PDFDisplay } from "./components/PDFDisplay";
 
-import { pdfjs } from 'react-pdf';
-
-import { ControlData, InvoiceData } from "./types";
-
-import 'react-pdf/dist/Page/TextLayer.css';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-
+import { ControlData } from "./types";
 
 function Application(){
     const [data, setData] = useState<ControlData>()
@@ -55,74 +49,6 @@ function Application(){
             </div>
 		</div>
 	)
-}
-
-function PDFDisplay({invoice}: {invoice : InvoiceData | undefined}){
-    const [page, setPage] = useState(1)
-    const [pdfview, enablePdfView] = useState(false); //TODO: change to true later
-
-    function nextPage(){
-        setPage(page+1);
-    }
-    function prevPage(){
-        setPage(page-1);
-    }
-    function togglePDFView() {
-        enablePdfView(!pdfview);
-    }
-
-    pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.min.mjs`;
-    if (pdfview) {
-        return (
-            <div className="h-full bg-blue-200 pt-1.5 pb-1.5 pl-4 pr-4">
-                <div className="mx-auto flex flex-col">
-                    <div className="mx-auto">
-                    <label>
-                        <input type="checkbox" name="PDFViewToggle" checked={pdfview} onChange={togglePDFView}></input> PDF Ansicht
-                    </label>
-                    </div>
-                    <Document file="/pdf">
-                        <Page pageNumber={page} />
-                    </Document>
-                    <div className="flex flex-row pt-2">
-                        <button className="mx-auto" onClick={prevPage}>Vorherige Seite</button>
-                        <button className="mx-auto" onClick={nextPage}>Nächste Seite</button>
-                    </div>
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div className="min-w-full flex flex-col pt-2">
-            <label className="mx-auto"><input type="checkbox" name="PDFViewToggle" onChange={togglePDFView}></input> PDF Ansicht</label>
-            <p>Dataview</p>
-            <div className="px-4">
-            <div className="grid grid-cols-2">
-                <p className="px-4">Sender:<br/>
-                   {invoice?.sender?.name}<br/> 
-                   {invoice?.sender?.street}<br/>
-                   {invoice?.sender?.zip}<br/>
-                   {invoice?.sender?.location}<br/>
-                   {invoice?.sender?.country}<br/>
-                </p>
-                <p className="px-4">Empfänger:<br/>
-                   {invoice?.reciever?.name}<br/>
-                   {invoice?.reciever?.street}<br/>
-                   {invoice?.reciever?.zip}<br/>
-                   {invoice?.reciever?.location}<br/>
-                   {invoice?.reciever?.country}<br/>
-                </p>
-            </div>
-            <div className="pt-2 flex flex-col gap-y-2">
-                <p>Rechnungsnummer: {invoice?.invoiceNumber}</p>
-                {invoice?.sellerVATID ? <p>Verkäufers VAT ID: {invoice?.sellerVATID}</p> : ""}
-                {invoice?.sellerTaxID ? <p>Verkäufers Steuer ID: {invoice?.sellerTaxID}</p> : ""}
-                <p>Rechnungsbetrag: {invoice?.invoiceTotal} {invoice?.currency}</p>
-            </div>
-            </div>
-            </div>
-        )
-    }
 }
 
 const root = createRoot(document.getElementById('app'));
